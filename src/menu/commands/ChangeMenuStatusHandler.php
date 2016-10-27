d6a7592003747a102cb3f02596167c16f3026c6f
rokorolov <rokorolov@gmail.com>
2016-10-27 20:00:27 +0300

    Change menu status recursive with children

--- src/menu/commands/ChangeMenuStatusHandler.php
+++ src/menu/commands/ChangeMenuStatusHandler.php
@@ -4,6 +4,7 @@ namespace rokorolov\parus\menu\commands;
 
 use rokorolov\parus\menu\repositories\MenuRepository;
 use rokorolov\parus\admin\exceptions\LogicException;
+use Yii;
 
 /**
  * ChangeMenuStatusHandler
@@ -25,9 +26,27 @@ class ChangeMenuStatusHandler
         if (null === $menu = $this->menuRepository->findById($command->getId())) {
             throw new LogicException('Menu does not exist.');
         }
+
+        $status = $command->getStatus();
+        $menu->status = $status;
+        $childrens = $this->menuRepository->findChildren($menu);
         
-        $menu->status = $command->getStatus();
-        $this->menuRepository->update($menu);
+        $transaction = Yii::$app->db->beginTransaction();
+
+        try {
+            $this->menuRepository->update($menu);
+            
+            foreach($childrens as $children) {
+                if ($children->status !== $status) {
+                    $children->status = $status;
+                    $this->menuRepository->update($children);
+                }
+            }
+            $transaction->commit();
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            throw $e;
+        }
     }
 
 }
