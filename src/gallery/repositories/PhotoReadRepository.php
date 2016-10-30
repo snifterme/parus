4207a44ef444c990685ee90270ab1bca08d40271
rokorolov <rokorolov@gmail.com>
2016-10-30 10:43:56 +0200

    Add feature to eager load album photos in BaseReadRepository

--- src/gallery/repositories/PhotoReadRepository.php
+++ src/gallery/repositories/PhotoReadRepository.php
@@ -36,18 +36,10 @@ class PhotoReadRepository extends BaseReadRepository
     public function findByAlbumId($id, array $with = [])
     {
         $photos = $this->orderBy('order')->findManyBy('album_id', $id);
-
-        if (in_array('translations', $with)) {
-            $ids = ArrayHelper::getColumn($photos, 'id');
-            $translations = ArrayHelper::index($this->findTranslations($ids), null, 'photo_id');
-            foreach ($photos as $photo) {
-                if (isset($translations[$photo->id])) {
-                    $photo->translations = $translations[$photo->id];
-                }
-            }
-        }
         
-        $this->reset();
+        if (in_array('translations', $with)) {
+            $this->eagerPopulateTranslations($photos);
+        }
         
         return $photos;
     }
@@ -96,14 +88,25 @@ class PhotoReadRepository extends BaseReadRepository
             ->leftJoin(PhotoLang::tableName() . ' pl', 'pl.photo_id = p.id');
     }
     
-    protected function populateTranslation($album, &$data)
+    protected function populateTranslation($photo, &$data)
     {
-        $album->translation = new PhotoLangDto($data, self::TABLE_SELECT_PREFIX_PHOTO_LANG);
+        $photo->translation = new PhotoLangDto($data, self::TABLE_SELECT_PREFIX_PHOTO_LANG);
     }
     
-    protected function populateTranslations($album)
+    protected function populateTranslations($photo)
     {
-        $album->translations = $this->findTranslations($album->id);
+        $photo->translations = $this->findTranslations($photo->id);
+    }
+    
+    public function eagerPopulateTranslations($photos)
+    {
+        $ids = ArrayHelper::getColumn($photos, 'id');
+        $translations = ArrayHelper::index($this->findTranslations($ids), null, 'photo_id');
+        foreach ($photos as $photo) {
+            if (isset($translations[$photo->id])) {
+                $photo->translations = $translations[$photo->id];
+            }
+        }
     }
     
     public function selectAttributesMap()
